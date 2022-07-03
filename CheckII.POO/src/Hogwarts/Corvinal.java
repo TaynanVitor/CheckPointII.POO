9ce32e2479059e75f83edb29bf18e0b0eb2a9b53
TaynanVitor <89108201+TaynanVitor@users.noreply.github.com>
2022-07-03 10:50:56 -0300

    New file

---
+++ CheckII.POO/src/Hogwarts/Corvinal.java
@@ -0,0 +1,37 @@
+package Hogwarts;
+
+public class Corvinal extends Casas {
+
+    public Corvinal(String orientador, Integer qtdAlunos, Integer pontuacaoCasa, String especialidade) {
+        super(orientador, qtdAlunos, pontuacaoCasa, especialidade);
+    }
+
+    @Override
+    public int obterQuantidadeDeAlunos() throws AlunosException {
+        int quantidadeDeAlunos = 0;
+        for (Alunos aluno : listaAlunos) {
+            if (aluno.getDisciplinaDestaque() == "poções" || aluno.getAnoLetivo() <=7) {
+                quantidadeDeAlunos++;
+            } else {
+                throw new AlunosException("Aluno já formado");
+            }
+            System.out.println("A quantidadde de alunos nessa casa é de: "+ quantidadeDeAlunos);
+        }
+
+        return quantidadeDeAlunos;
+    }
+
+
+
+    @Override
+    public int somarPontos () {
+        int total = 0;
+        for (Alunos pontuacao : getListaAlunos()) {
+            if (pontuacao.getDisciplinaDestaque() == "poções") {
+                total += pontuacao.getPontuacao();
+            }
+        }
+        return total;
+    }
+
+}
